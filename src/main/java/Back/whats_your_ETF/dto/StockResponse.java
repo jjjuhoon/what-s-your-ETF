7d9c890ed33ed0badd6eedda35f5f2cc5cd3c894
greenlemonT <ms950219@naver.com>
2024-11-18 13:50:44 +0900

    feat: getUserETFlist (#5)

--- src/main/java/Back/whats_your_ETF/dto/StockResponse.java
+++ src/main/java/Back/whats_your_ETF/dto/StockResponse.java
@@ -1,14 +1,7 @@
 package Back.whats_your_ETF.dto;
 
-import lombok.*;
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-@Builder
-public class StockResponse {
-
-    private String stockCode;
-    private String stockName;
-    private Double percentage;
-}
+public record StockResponse(
+        String stockCode,
+        String stockName,
+        Double percentage
+) {}
