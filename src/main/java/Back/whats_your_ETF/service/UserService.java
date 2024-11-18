23b6bfb03a3a5522ed7f7884a6ff233f7ad43932
cie10 <leesky0075@dgu.ac.kr>
2024-11-18 13:50:14 +0900

    feat: add user membership service(#7)

--- src/main/java/Back/whats_your_ETF/service/UserService.java
+++ src/main/java/Back/whats_your_ETF/service/UserService.java
@@ -2,6 +2,8 @@ package Back.whats_your_ETF.service;
 
 import Back.whats_your_ETF.entity.User;
 import Back.whats_your_ETF.repository.UserRepository;
+import jakarta.activation.MimeTypeParameterList;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -18,4 +20,19 @@ public class UserService {
     public Optional<User> getUserById(Long userId) {
         return userRepository.findById(userId);
     }
+
+    //membership update
+    @Transactional
+    public boolean updateMembership(Long userId){
+        Optional<User> optionalUser = userRepository.findById(userId);
+        
+        if(optionalUser.isPresent()){
+            User user = optionalUser.get();
+            user.setMember(true); // 회원 상태를 true로 변경
+            userRepository.save(user); // 변경된 데이터를 저장
+            return true;
+        }
+        return false; //사용자 ID가 없을 경우
+    }
+
 }
