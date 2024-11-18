7d2b22ac861f3744481c194e040b0ff18fcdeb1c
cie10 <leesky0075@dgu.ac.kr>
2024-11-18 13:49:57 +0900

    feat: edit url and implement user membership API(#7)

--- src/main/java/Back/whats_your_ETF/controller/UserController.java
+++ src/main/java/Back/whats_your_ETF/controller/UserController.java
@@ -10,13 +10,13 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api")
+@RequestMapping("/api/userinfo")
 public class UserController {
 
     private final UserService userService;
 
     // 사용자 정보 불러오기
-    @GetMapping("/userinfo/{user_id}")
+    @GetMapping("/{user_id}")
     public ResponseEntity<UserResponse> getUserInfo(@PathVariable("user_id") Long userId) {
         return userService.getUserById(userId)
                 .map(user -> ResponseEntity.ok(new UserResponse(
@@ -29,5 +29,17 @@ public class UserController {
                         user.getSubscriberCount()
                 )))
                 .orElse(ResponseEntity.notFound().build());
+    }
+
+    //멤버십 가입하기
+    @PostMapping("/membership/{user_id}")
+    public ResponseEntity<String> updateMembership(@PathVariable("user_id") Long userId) {
+        boolean updated = userService.updateMembership(userId);
+
+        if (updated) {
+            return ResponseEntity.ok("회원 상태로 업데이트되었습니다.");
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
