c19916d887a2decd829abe62cc960d7e2105510b
cie10 <leesky0075@dgu.ac.kr>
2024-11-18 09:31:03 +0900

    feat: create Entity (#2)

---
+++ src/main/java/Back/whats_your_ETF/entity/Notice.java
@@ -0,0 +1,27 @@
+package Back.whats_your_ETF.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "notice")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Notice {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "notice_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(name = "content", nullable = false)
+    private String content;
+}
