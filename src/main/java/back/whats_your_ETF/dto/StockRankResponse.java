a4cc5353848132dbcb098a5130ef6b2e01548c9d
greenlemonT <ms950219@naver.com>
2024-11-27 15:34:37 +0900

    fix: change column type of Ranking

--- src/main/java/back/whats_your_ETF/dto/StockRankResponse.java
+++ src/main/java/back/whats_your_ETF/dto/StockRankResponse.java
@@ -3,5 +3,6 @@ package back.whats_your_ETF.dto;
 public record StockRankResponse (
         String stockCode,
         String stockName,
-        Double price
+        Double price,
+        Double percentage
 ) {}
